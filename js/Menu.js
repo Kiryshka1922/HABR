79d680753990b1166d367afba48b916b19cc063d
Kirill <kipabelousov@edu.hse.ru>
2024-11-17 22:17:49 +0300

    My first Work-Web

--- js/Menu.js
+++ js/Menu.js
@@ -1,6 +1,4 @@
-// console.log ("Саламус");
-// const but1 = document.querySelector("menu-botomn")
-// const but2 = document.querySelector("Pressing")
+
 function clikevent1() {
     let element = document.getElementById('butt');
     const element1=document.getElementById('123')
@@ -14,8 +12,7 @@ function clikevent1() {
     if (element1.style.display === 'flex') {
       element1.style.display = 'none';
       element.style.display = 'block';
-    // } else {
-    //   element1.style.display = 'none';
+  
  }
 }
  
