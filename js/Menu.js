d8c8aad503918b5050957c3f93b6124374353156
Kirill <kipabelousov@edu.hse.ru>
2024-11-17 22:13:50 +0300

    My first Work-Web

---
+++ js/Menu.js
@@ -0,0 +1,21 @@
+// console.log ("Саламус");
+// const but1 = document.querySelector("menu-botomn")
+// const but2 = document.querySelector("Pressing")
+function clikevent1() {
+    let element = document.getElementById('butt');
+    const element1=document.getElementById('123')
+    if (element1.style.display === 'none') {
+      element1.style.display = 'flex';
+      element.style.display = 'none';}
+    }
+  function clikevent2() {
+    let element = document.getElementById('butt');
+    const element1=document.getElementById('123')
+    if (element1.style.display === 'flex') {
+      element1.style.display = 'none';
+      element.style.display = 'block';
+    // } else {
+    //   element1.style.display = 'none';
+ }
+}
+ 
